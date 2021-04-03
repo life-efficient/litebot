1654b5b8ff0c464a9f2c1b8c7312e1a0ca0c25e1
Harry Berg <harryaberg@gmail.com>
2021-04-03 12:53:58 +0100

    restructured to become pypi package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='bots',
+    version='0.0.0',    
+    description='Boilerplate code for building bots',
+    url='https://github.com/life-efficient/bots',
+    author='Harry Berg',
+    author_email='harryaberg@gmail.com',
+    license='MIT',
+    packages=[
+        'bots',
+    ],
+    install_requires=[],
+    keywords = ['scraping', 'bots'], 
+    classifiers=[
+    ],
+)
